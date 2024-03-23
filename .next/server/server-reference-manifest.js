e0bb4bdffefa670659cef8c6321b208e2306f373
arnav-ag <arnav.ctf@gmail.com>
2024-03-24 02:33:58 +0800

    shift-enter and breakpoints

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"MPU0HyTPe1i2ymbgeoRC9p3P3SCdNGKcG94yDG2VepM=\"\n}"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,21 @@ const HomePage = () => {
   const [editorValue, setEditorValue] = useState<string | undefined>('');
   const [output, setOutput] = useState<string | undefined>('');
   const [isSideBySide, setIsSideBySide] = useState(false);
+  const [breakpoints, setBreakpoints] = useState<number[]>([]);
   const fileInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const editorValue = localStorage.getItem('editorValue') || '';
+    setEditorValue(editorValue);
+  }, []);
+
   const handleRun = () => {
+    console.log('Breakpoints at lines:', breakpoints);
     setOutput(editorValue);
   };
   const handleEditorChange = (value: string | undefined) => {
+    // save to local storage
+    localStorage.setItem('editorValue', value || '');
     setEditorValue(value);
   };
 
@@ -22,7 +32,7 @@ const HomePage = () => {
     fileReader.readAsText(file, 'UTF-8');
     fileReader.onload = e => {
       const result = e.target?.result as string;
-      setEditorValue(result || '');
+      handleEditorChange(result);
     };
   };
 
@@ -33,6 +43,10 @@ const HomePage = () => {
     link.download = 'editorContent.txt';
     link.click();
     URL.revokeObjectURL(link.href);
+  };
+
+  const handleBreakpointsChange = (newBreakpoints: number[]) => {
+    setBreakpoints(newBreakpoints);
   };
 
   useEffect(() => {
@@ -51,7 +65,7 @@ const HomePage = () => {
           className="bg-[#4CAF50] hover:bg-[#66BB6A] text-white py-1 px-2 rounded text-sm"
           onClick={handleRun}
         >
-          Run
+          Run (Shift + Enter)
         </button>
         <button
           className="bg-[#FFA726] hover:bg-[#FFB74D] text-white py-1 px-2 rounded text-sm"
@@ -78,7 +92,12 @@ const HomePage = () => {
           className={`flex flex-1 ${isSideBySide ? 'h-full w-1/2' : 'w-full'}`}
           style={{ minHeight: isSideBySide ? 'auto' : '50%' }}
         >
-          <MonacoEditor onChange={handleEditorChange} value={editorValue} />
+          <MonacoEditor
+            onChange={handleEditorChange}
+            value={editorValue}
+            onBreakpointsChange={handleBreakpointsChange}
+            onRun={handleRun}
+          />{' '}
         </div>
         <div
           className={`flex flex-1 ${isSideBySide ? 'h-full w-1/2' : 'w-full'}`}
